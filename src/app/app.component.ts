f6de44d8a76cb1b3cc30061470570a6bd34c04a5
jason zheng <tso1158687@gmail.com>
2021-08-17 01:06:29 +0800

    add style and shi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'shihu';
+  currentIndex = 0;
+  shiList = [
+    {
+      title: '錦瑟',
+      author: '李商隱',
+      content: [
+        '錦瑟無端五十弦，一弦一柱思華年。',
+        '莊生曉夢迷蝴蝶，望帝春心托杜鵑。',
+        '滄海月明珠有淚，藍田日暖玉生煙。',
+        '此情可待成追憶，只是當時已惘然。',
+      ],
+    },
+    {
+      title: '題西林壁',
+      author: '蘇軾',
+      content: [
+        '橫看成嶺側成峰，遠近高低各不同。',
+        '不識廬山真面目，只緣身在此山中。',
+      ],
+    },
+  ];
+  next(): void {
+    this.currentIndex++;
+  }
 }
